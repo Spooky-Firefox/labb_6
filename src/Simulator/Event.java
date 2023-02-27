f93dea6615ce821ca2160c2f0ca156cabd271260
yaboiyomokoishere <kalhed-2@student.ltu.se>
2023-02-27 17:31:11 +0100

    Update Event.java

--- src/Simulator/Event.java
+++ src/Simulator/Event.java
@@ -1,11 +1,17 @@
 package Simulator;
 
-public abstract class Event
-{
-	public int startTime;
+import Simulator.EventQueue;
+public abstract class Event{
+	private int startTime;
+	private EventQueue queue;
+	
+	public Event(int time, EventQueue eventQueue) {
+		this.queue = eventQueue;
+		this.startTime = time;
+	}
 	
 	public void execute(State state)
 	{
-		
+		this.addEvent();
 	}
 }
