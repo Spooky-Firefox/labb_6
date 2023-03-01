8cdecb1717278c833528d6964929a6dc35d8b497
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 17:36:39 +0100

    fix, add way to event to be added if it happens after last event

--- src/Simulator/EventQueue.java
+++ src/Simulator/EventQueue.java
@@ -3,27 +3,35 @@ package Simulator;
 import java.util.ArrayList;
 
 public class EventQueue {
-	ArrayList<Event> list = new ArrayList<>();
+    ArrayList<Event> list = new ArrayList<>();
 
-	public void addEvent(Event event) {
-		// TODO throw error if the time is in the past
-		if(list.size()==0){
-			list.add(event);
-		} else {
-			for (int i = 0; i < list.size(); i++) {
-				if(event.getStartTime()<list.get(i).getStartTime()){
-					list.add(i,event);
-					break;
-				}
-			}
-		}
-	}
+    public void addEvent(Event event) {
+        // TODO throw error if the time is in the past
 
-	public Event nextEvent() {
-		return list.remove(0);
-	}
+        // if the que is empty add the event
+        if (list.size() == 0) {
+            list.add(event);
 
-	public boolean hasNext() {
-		return list.isEmpty();
-	}
+            // if the event is after the last element
+        } else if (event.getStartTime() > list.get(list.size() - 1).getStartTime()) {
+            list.add(event);
+
+            // if the event is before last element, find which is right after and add it att that index
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                if (event.getStartTime() < list.get(i).getStartTime()) {
+                    list.add(i, event);
+                    break;
+                }
+            }
+        }
+    }
+
+    public Event nextEvent() {
+        return list.remove(0);
+    }
+
+    public boolean hasNext() {
+        return list.isEmpty();
+    }
 }
