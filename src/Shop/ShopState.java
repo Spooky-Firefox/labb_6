6fe56440c471b1e70f393f5715d582c15d6467b2
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 18:03:23 +0100

    add method updateStatistics NOT IMPLEMENTED, with comment of what needs to be done

--- src/Shop/ShopState.java
+++ src/Shop/ShopState.java
@@ -5,4 +5,13 @@ import Simulator.State;
 public class ShopState extends State
 {
 	public boolean open;
+	public void updateStatistics(){
+		// TODO implement updating of statistics and notify observers of such change
+		// sum of time of empty checkouts
+		// sum of total time in checkout queue
+		// for the sum of checkout queue time and total time of empty checkouts
+		// delta time is required
+		// a example of calculating the sum queue time
+		// queueTime = queueTime + checkoutQueue.size()*deltaTime
+	}
 }
