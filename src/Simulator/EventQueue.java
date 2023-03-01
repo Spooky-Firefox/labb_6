faa75ae8e4d4543cd45069327b40f2b0b8544288
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 17:27:50 +0100

    add addEvent at correct time

--- src/Simulator/EventQueue.java
+++ src/Simulator/EventQueue.java
@@ -6,7 +6,17 @@ public class EventQueue {
 	ArrayList<Event> list = new ArrayList<>();
 
 	public void addEvent(Event event) {
-		list.add(event);
+		// TODO throw error if the time is in the past
+		if(list.size()==0){
+			list.add(event);
+		} else {
+			for (int i = 0; i < list.size(); i++) {
+				if(event.getStartTime()<list.get(i).getStartTime()){
+					list.add(i,event);
+					break;
+				}
+			}
+		}
 	}
 
 	public Event nextEvent() {
