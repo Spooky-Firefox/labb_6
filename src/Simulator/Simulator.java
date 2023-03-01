33c8ecefb28d76fcc204027c3cb3b3ce6c1eaed8
yaboiyomokoishere <kalhed-2@student.ltu.se>
2023-03-01 18:17:13 +0100

    Update Simulator.java

--- src/Simulator/Simulator.java
+++ src/Simulator/Simulator.java
@@ -14,6 +14,7 @@ public class Simulator {
 		while(state.isStopped() == false) {
 			events.nextEvent().execute(state);
 			if (!events.hasNext()) {
+				System.out.println("event was null");
 				state.stop();
 			}
 		}
