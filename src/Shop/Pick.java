83d300548c1c2d07dbd07677802a230342d74463
yaboiyomokoishere <kalhed-2@student.ltu.se>
2023-03-01 19:34:27 +0100

    Update Pick.java

--- src/Shop/Pick.java
+++ src/Shop/Pick.java
@@ -4,11 +4,22 @@ import Simulator.EventQueue;
 import Simulator.State;
 
 public class Pick extends ShopEvent {
+	private int customerNumber;
 	public Pick(int time, EventQueue eventQueue) {
 		super(time, eventQueue);
+	}
+	public Pick(int startTime, int customerNumber, EventQueue eventQueue) {
+		super(startTime, eventQueue);
+		this.customerNumber = customerNumber;
 	}
 
 	@Override
 	public void execute(ShopState state) {
+		if (state.freeCheckout()) {
+			Payment payment = new Payment(super.getStartTime() + 1, super.getQueue());
+		}
+		else {
+			state.queue.addCustomer(this.customerNumber);
+		}
 	}
 }
