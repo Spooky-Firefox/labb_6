e50c78cc2a79b511ab6fc4b7e94df26908a6e41e
Olle Ronstad <olle.ronstad@gmail.com>
2023-03-01 17:27:14 +0100

    add getStartTime()

--- src/Simulator/Event.java
+++ src/Simulator/Event.java
@@ -1,14 +1,16 @@
 package Simulator;
 
-public abstract class Event{
-	private int startTime;
-	private EventQueue queue;
+public abstract class Event {
+    private int startTime;
+    private EventQueue queue;
 
-	public Event(int time, EventQueue eventQueue) {
-		this.queue = eventQueue;
-		this.startTime = time;
-		eventQueue.addEvent(this);
-	}
-
-	public abstract void execute(State state);
+    public Event(int time, EventQueue eventQueue) {
+        this.queue = eventQueue;
+        this.startTime = time;
+        eventQueue.addEvent(this);
+    }
+    public int getStartTime(){
+        return startTime;
+    }
+    public abstract void execute(State state);
 }
