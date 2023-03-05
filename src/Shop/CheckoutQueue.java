0fcb33e203f0ddf8d586f10057dc13b1845f25a9
Alex <github@larus.se>
2023-03-05 18:21:27 +0100

    Adds size() to CheckoutQueue and reformat tabs

--- src/Shop/CheckoutQueue.java
+++ src/Shop/CheckoutQueue.java
@@ -3,26 +3,30 @@ package Shop;
 import java.util.ArrayList;
 
 public class CheckoutQueue {
-    private ArrayList<Integer> customers;
+	private ArrayList<Integer> customers;
 
-    public CheckoutQueue() {
-        customers = new ArrayList<>();
-    }
+	public CheckoutQueue() {
+		customers = new ArrayList<>();
+	}
 
-    public void addCustomer(int customersNumber) {
-        this.customers.add(customersNumber);
-    }
+	public void addCustomer(int customersNumber) {
+		this.customers.add(customersNumber);
+	}
 
-    public int nextCustomer() {
-        return this.customers.remove(0);
-    }
+	public int nextCustomer() {
+		return this.customers.remove(0);
+	}
 
-    public boolean hasNext() {
-        return !this.customers.isEmpty();
-    }
+	public boolean hasNext() {
+		return !this.customers.isEmpty();
+	}
 
-    @Override
-    public String toString() {
-        return this.customers.toString();
-    }
+	public int size() {
+		return customers.size();
+	}
+
+	@Override
+	public String toString() {
+		return this.customers.toString();
+	}
 }
