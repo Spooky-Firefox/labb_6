977200716c977e9c89ea21498ff8a17ce62ac43b
Alex <github@larus.se>
2023-03-05 19:46:55 +0100

    Reformats tabs in some files...
    We should really  use an autoformatter?

--- src/System/EventQueue.java
+++ src/System/EventQueue.java
@@ -3,46 +3,46 @@ package System;
 import java.util.ArrayList;
 
 public class EventQueue {
-    ArrayList<Event> list;
-    State state;
-    public EventQueue(State state){
-        this.state = state;
-        this.list = new ArrayList<>();
-    }
-    public void addEvent(Event event) {
-        // throw error if the time is in the past
-        if (event.getStartTime() < this.state.getTime()){
-            throw new IllegalArgumentException("Can't add a event that happens in the past");
-        }
+	ArrayList<Event> list;
+	State state;
+	public EventQueue(State state){
+		this.state = state;
+		this.list = new ArrayList<>();
+	}
+	public void addEvent(Event event) {
+		// throw error if the time is in the past
+		if (event.getStartTime() < this.state.getTime()){
+			throw new IllegalArgumentException("Can't add a event that happens in the past");
+		}
 
 		// if the queue is empty add the event
-        if (list.size() == 0) {
-            list.add(event);
-            return;
+		if (list.size() == 0) {
+			list.add(event);
+			return;
 		}
 
 		// if the event is after the last element
-        if (event.getStartTime() > list.get(list.size() - 1).getStartTime()) {
-            list.add(event);
-            return;
+		if (event.getStartTime() > list.get(list.size() - 1).getStartTime()) {
+			list.add(event);
+			return;
 		}
 
-        // if the event is before last element,
-        // find which is right after and add it att that index
-        for (int i = 0; i < list.size(); i++) {
-            if (event.getStartTime() < list.get(i).getStartTime()) {
-                list.add(i, event);
-                break;
-            }
-        }
-    }
+		// if the event is before last element,
+		// find which is right after and add it att that index
+		for (int i = 0; i < list.size(); i++) {
+			if (event.getStartTime() < list.get(i).getStartTime()) {
+				list.add(i, event);
+				break;
+			}
+		}
+	}
 
-    public Event nextEvent() {
+	public Event nextEvent() {
 		if (hasNext()) {
-    		return list.remove(0);
+			return list.remove(0);
 		}
 		return null;
-    }
+	}
 
 	public boolean hasNext() {
 		return !list.isEmpty();
