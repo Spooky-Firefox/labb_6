410366c3ffb36f6889d51b413c7adebd957c2956
Alex <github@larus.se>
2023-03-08 18:59:17 +0100

    Cleans up and documents Optimize

--- src/Main/Optimize.java
+++ src/Main/Optimize.java
@@ -1,20 +1,25 @@
 package Main;
 
 import Controller.*;
-import Shop.EventClosing;
-import Shop.EventStart;
 import Shop.ShopState;
+import Shop.EventStart;
+import Shop.EventClosing;
 
 import java.util.Random;
 
 public class Optimize {
-	private static ShopState runOneSim( // method one
-								 int openCheckouts, int maxCustomers, double arrivalTime, double pickTimeMin,
-								 double pickTimeMax, double paymentTimeMin, double paymentTimeMax, long rngSeed) {
+
+	// Method one: run one simulation with fixed params, but don't print output and
+	// return end state.
+	private static ShopState runOneSim(
+			int openCheckouts, int maxCustomers, double arrivalTime, double pickTimeMin,
+			double pickTimeMax, double paymentTimeMin, double paymentTimeMax, long rngSeed) {
+
 		// Sets up initial instances
 		ShopState shopState = new ShopState(
-				openCheckouts, maxCustomers, arrivalTime, pickTimeMin, pickTimeMax,
-				paymentTimeMin, paymentTimeMax, rngSeed);
+			openCheckouts, maxCustomers, arrivalTime, pickTimeMin, pickTimeMax,
+			paymentTimeMin, paymentTimeMax, rngSeed
+		);
 		EventQueue eventQueue = new EventQueue(shopState);
 
 		// Adds initial events
@@ -25,49 +30,51 @@ public class Optimize {
 		Event eventClose = new EventClosing(10, eventQueue);
 		eventQueue.addEvent(eventClose);
 
-		// Sets up and runs the simulator
+		// Sets up and runs the simulator silently
 		Simulator sim = new Simulator(shopState, eventQueue);
 		sim.runLoop();
-
 		return shopState;
 	}
 
-	private static int findMinCheckoutFromSeed( // method two
-										 int maxCustomers, double arrivalTime, double pickTimeMin,
-										 double pickTimeMax, double paymentTimeMin, double paymentTimeMax, long rngSeed) {
-		// TODO: make binary search
-
+	// Method two: Using first method, run multiple, sequential simulations and find the
+	// least amount of checkouts that causes the least amount of missed customers.
+	// It returns the optimal number of checkouts.
+	private static int findMinCheckoutFromSeed(
+			int maxCustomers, double arrivalTime, double pickTimeMin,
+			double pickTimeMax, double paymentTimeMin, double paymentTimeMax, long rngSeed) {
 
 		// if we have a Checkout for each customer, it doesn't help with more
 		int maxCheckouts = maxCustomers;
 		int minCheckouts = maxCustomers;
 		int minMissedCustomers = Integer.MAX_VALUE;
-		for (int i = maxCheckouts; i >0; i--) {
+		for (int i = maxCheckouts; i > 0; i--) {
 			ShopState state = runOneSim(
-					i, maxCustomers, arrivalTime, pickTimeMin, pickTimeMax, paymentTimeMin, paymentTimeMax, rngSeed);
-//			System.out.println("\n\n");
-//			System.out.println("checkouts "+state.openCheckouts);
-//			System.out.println("missed "+state.customersMissed);
+				i, maxCustomers, arrivalTime, pickTimeMin, pickTimeMax, paymentTimeMin,
+				paymentTimeMax, rngSeed
+			);
 			if (state.customersMissed <= minMissedCustomers) {
 				minMissedCustomers = state.customersMissed;
 				minCheckouts = i;
 			}
 		}
-//		System.out.println("minMissed "+minMissedCustomers);
 		return minCheckouts;
 	}
 
-	private static int findMinCheckout( // method tre
-								 int maxCustomers, double arrivalTime, double pickTimeMin,
-								 double pickTimeMax, double paymentTimeMin, double paymentTimeMax, long sourceRand) {
-		Random rand = new Random(sourceRand);
+	// Method three: Using the second method, run even more simulations using randomized
+	// seed values to find an optimal amount of free checkouts that works the best (over
+	// 100 loops). It returns the best amount of checkouts.
+	private static int findMinCheckout(
+			int maxCustomers, double arrivalTime, double pickTimeMin, double pickTimeMax,
+			double paymentTimeMin, double paymentTimeMax, long sourceRand) {
 
+		Random rand = new Random(sourceRand);
 		int maxCheckouts = 0;
 		int stepsSinceChange = 0;
 		while (stepsSinceChange <= 100) {
 			int minCheckouts = findMinCheckoutFromSeed(
-					maxCustomers, arrivalTime, pickTimeMin, pickTimeMax,
-					paymentTimeMin, paymentTimeMax, rand.nextLong());
+				maxCustomers, arrivalTime, pickTimeMin, pickTimeMax, paymentTimeMin,
+				paymentTimeMax, rand.nextLong()
+			);
 			if (minCheckouts > maxCheckouts){
 				maxCheckouts = minCheckouts;
 				stepsSinceChange = 0;
@@ -78,10 +85,7 @@ public class Optimize {
 		return maxCheckouts;
 	}
 
-
 	public static void main(String[] args) {
-		System.out.println(findMinCheckout(
-				5,1,0.5,1,
-				2,3,1234));
+		System.out.println(findMinCheckout(5,1,0.5,1,2,3,1234));
 	}
 }
